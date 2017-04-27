d1df7947c1a98b207f0c53206820168a05f6c52a
H3rN4n <desouza030689@gmail.com>
2017-04-27 18:06:30 -0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,87 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  mock = {
+    section: [
+      {
+        block: [
+          {
+            "orientation": "flex-row",
+            content: [
+              {
+                width: 1,
+                height: 2
+              },
+              {
+                width: 1,
+                height: 2
+              },
+              {
+                width: 2,
+                height: 2
+              },
+              {
+                width: 2,
+                height: 1
+              },
+              {
+                width: 1,
+                height: 1
+              },
+              {
+                width: 1,
+                height: 1
+              },
+            ]
+          },
+          //
+          // {
+          //   "orientation": "flex-column",
+          //   content: [
+          //     {
+          //       width: 1,
+          //       height: 2
+          //     },
+          //     {
+          //       width: 1,
+          //       height: 2
+          //     },
+          //     {
+          //       width: 2,
+          //       height: 2
+          //     },
+          //     {
+          //       width: 2,
+          //       height: 3
+          //     },
+          //   ]
+          // }
+        ]
+      }
+    ]
+  }
+
+  options = {
+    widths: [
+      '0', '25%', '50%', '100%'
+    ],
+    heights: [
+      '0', '45%', '55%'
+    ]
+  };
+
+  private setContentWidth(input) {
+    return this.options.widths[input];
+  }
+
+  private setContentHeights(input) {
+    return this.options.heights[input];
+  }
+
+  public buildStyles(content) {
+    return { 'width': this.setContentWidth(content.width), 'height': this.setContentHeights(content.height) }
+    //return { 'width': this.setContentWidth(content.width) }
+  }
 }
+
